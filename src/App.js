2b240dd51f3799c6e3ed9f5c463ff76986fc2c90
Kles <klesta.luli@gmail.com>
2022-04-08 17:17:49 +0300

    set up die component to take props & manually set up 10 instances of dice

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from "react"
 import "./style.css"
+import Die from "./Die"
 
 export default function App() {
     return (
         <main className="App">
-            
+            <Die value="1" />
+            <Die value="1" />
+            <Die value="1" />
+            <Die value="1" />
+            <Die value="1" />
+            <Die value="1" />
+            <Die value="1" />
+            <Die value="1" />
+            <Die value="1" />
+            <Die value="6" />
         </main>
     )
 }
